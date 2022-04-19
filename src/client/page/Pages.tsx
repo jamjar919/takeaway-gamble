ab3fc1465ced41365f09bea5943df23213ae66e8
James Paterson <jpaterson@tripadvisor.com>
2022-04-19 23:08:37 +0100

    better results paghe

--- src/client/page/Pages.tsx
+++ src/client/page/Pages.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import {useAppContext} from "../context/AppContext";
 import {GambleResultPage} from "./gamble-result/GambleResultPage";
 import {SearchPage} from "./search/SearchPage";
-import {mockGambleResponse} from "../../common/spec/mock/MockGambleResponse";
 
 const Pages: React.FC = () => {
     const {
         gamble,
+        gambleResult
     } = useAppContext();
-
-    const gambleResult = mockGambleResponse;
 
     return (
         <div>
