6afc5e5b6c4a2a124728fb6fb03504827d9724d2
James Paterson <jpaterson@tripadvisor.com>
2022-04-20 23:17:17 +0100

    logging

--- src/server/gamble/gamble.ts
+++ src/server/gamble/gamble.ts
@@ -114,8 +114,11 @@ export const gamble = async (req: Request<{}, GambleRequest>, res: Response) => 
         }
 
         sendJSON<SuccessfulGambleResponse>(response, res);
+
     } catch (e: any) {
         if (e?.message === "Captcha required") {
+
+            console.log("sending", e.html());
 
             sendJSON<RequiresCaptchaResponse>({
                 type: "requires_captcha",
