de5debbe0a2c7fe49ba11babb3fa2ff771f6c14a
James Paterson <jpaterson@tripadvisor.com>
2022-11-09 01:58:35 +0000

    We stan

--- dist/scripts/beforeinstall.sh
+++ dist/scripts/beforeinstall.sh
@@ -7,7 +7,7 @@ sudo npm install --global yarn
 
 # Install forever module
 # https://www.npmjs.com/package/forever
-sudo yarn global add forever
+yarn global add forever
 
 # Clean working folder
 sudo find /home/takeawaygamble -delete
