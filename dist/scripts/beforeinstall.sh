53daea403525c393616991323f61db065598911d
James Paterson <jpaterson@tripadvisor.com>
2022-11-09 01:27:19 +0000

    sudo

--- dist/scripts/beforeinstall.sh
+++ dist/scripts/beforeinstall.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Install node.js + yarn
-sudo curl --silent --location https://rpm.nodesource.com/setup_16.x | bash -
+sudo curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
 sudo yum -y install nodejs
-npm install --global yarn
+sudo npm install --global yarn
 
 # Install forever module
 # https://www.npmjs.com/package/forever
