2240b489123d1e4e5f3793ea35777ac18cb958a4
James Paterson <jpaterson@tripadvisor.com>
2022-11-15 16:20:08 +0000

    Increase memory limit for instance

--- dist/scripts/appstart.sh
+++ dist/scripts/appstart.sh
@@ -2,4 +2,4 @@
 
 # Stop all servers and start the server as a daemon
 pm2 stop takeaway-gamble
-pm2 start /home/takeawaygamble/server.js --max-memory-restart 2048 --name takeaway-gamble -- --max-old-space-size=2048
+pm2 start /home/takeawaygamble/server.js --max-memory-restart 2048M --name takeaway-gamble -- --max-old-space-size=2048
