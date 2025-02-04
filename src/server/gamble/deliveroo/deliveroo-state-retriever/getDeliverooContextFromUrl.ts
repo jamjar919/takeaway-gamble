017f42375f74b8b9387356a27af61f5d3c9506f8
James Paterson <jpaterson@tripadvisor.com>
2025-02-04 17:47:55 +0000

    Remove cheerio dep and upgrade node

--- src/server/gamble/deliveroo/deliveroo-state-retriever/getDeliverooContextFromUrl.ts
+++ src/server/gamble/deliveroo/deliveroo-state-retriever/getDeliverooContextFromUrl.ts
@@ -1,4 +1,3 @@
-import cheerio from "cheerio";
 import { DeliverooState } from "../../../type/deliveroo/DeliverooState";
 import { CaptchaRequiredError } from "../error/CaptchaRequiredError";
 import { doDeliverooFetch } from "../../../util/doDeliverooFetch";
@@ -16,32 +15,49 @@ const getDeliverooContextFromUrl = async (
         restaurants.text()
     );
 
-    const $ = cheerio.load(html);
-
-    const data = $("#__NEXT_DATA__");
-
-    if (!data || !data.html()) {
-        const html = $("html").html();
-        const title = $("title").text();
-
-        if (
-            title.indexOf("Cloudflare") > 0 ||
-            title.indexOf("Just a moment...") > 0
-        ) {
-            console.log("Captcha required - sending it back to the user");
-            throw new CaptchaRequiredError(String(html));
-        }
-
-        throw new Error(`State is null or undefined. State: ${data}`);
+    const title = getHtmlTagContentsFromString(html, "title");
+    if (title && title.includes("Cloudflare")) {
+        console.log("Captcha required - sending it back to the user");
+        throw new CaptchaRequiredError(String(html));
     }
 
     // Get the NEXT state + cookie
     try {
-        return JSON.parse(data.html() as string) as DeliverooState;
+        const nextData = getHtmlTagContentsFromString(html, 'script', `id="__NEXT_DATA__"`);
+
+        if (nextData === null) {
+            throw new Error("NEXT_DATA tag not present")
+        }
+
+        return JSON.parse(nextData) as DeliverooState;
     } catch (e: any) {
         console.log("Failed to find state", html);
         throw e;
     }
 };
+
+/**
+ * Retrieve the content of a tag from a string of HTML
+ *
+ * Eg, HTML = <hello>content</hello>
+ *     tag = hello
+ *          => "content"
+ *     HTML = <script id="something">content</script>,
+ *     tag = script
+ *     attributes = id="something"
+ *          => "content"
+ *
+ * This is a bit silly but so was pulling in an entire HTML parser to get the content of one tag
+ */
+const getHtmlTagContentsFromString = (html: string, tag: string, attributes: string = ''): string | null => {
+    const tagPattern = new RegExp(`<${tag}[^>]*${attributes}[^>]*>(.*?)</${tag}>`, 'gi');
+
+    const match = tagPattern.exec(html);
+    if (match !== null) {
+        return match[1];
+    }
+
+    return null;
+}
 
 export { getDeliverooContextFromUrl };
